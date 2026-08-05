ec1e4d0126e25d9e69a30eed48ce7b7b74c72e7a
Tj-Bots <tjbots123@gmail.com>
2026-08-05 18:24:51 +0300

    Update messages.py

--- Thunder/utils/messages.py
+++ Thunder/utils/messages.py
@@ -228,9 +228,9 @@ MSG_LINKS = (
     "> `{file_name}`\n\n"
     "📂 **File Size:** `{file_size}`\n\n"
     "🚀 **Download Link:**\n"
-    "> `{download_link}`\n\n"
+    "> {download_link}\n\n"
     "🖥️ **Stream Link:**\n"
-    "> `{stream_link}`\n\n"
+    "> {stream_link}\n\n"
     "⌛️ **Note: Links remain active while the bot is running and the file is accessible.**"
 )
 
@@ -255,8 +255,10 @@ MSG_PROCESSING_FILE = "⏳ **Processing your file...**"
 MSG_NEW_FILE_REQUEST = (
     "> 👤 **Source:** [{source_info}](tg://user?id={id_})\n"
     "> 🆔 **ID:** `{id_}`\n\n"
-    "🚀 **Download:** `{online_link}`\n\n"
-    "🖥️ **Stream:** `{stream_link}`"
+    "🚀 **Download:**\n"
+    "> {online_link}\n\n"
+    "🖥️ **Stream:**"
+    "> {stream_link}"
 )
 
 # ------ Batch Processing ------
